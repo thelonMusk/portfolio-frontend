e1edd17021b79cf331fb78d4d4a72e5ed1106f99
Aatiesh Srinivasan <you@example.com>
2025-11-18 13:27:50 +1300

    Add config.js to the root directory

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  API_URL: process.env.NODE_ENV === 'production' 
-    ? 'https://portfolio-backend1-fcev.onrender.com/api'
-    : 'http://localhost:5000/api'
-};
-
-export default config;
